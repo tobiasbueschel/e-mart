8ebadf9a4bf9a8e98a6be33b4895346a5498e021
Tobias Büschel <tobias.buschel.15@ucl.ac.uk>
2016-01-27 21:32:15 +0000

    update

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -11,10 +11,15 @@ function config($stateProvider, $urlRouterProvider) {
 
     $stateProvider
 
+        .state('login', {
+            url: "/login",
+            templateUrl: "views/login.html",
+            data: { pageTitle: 'Login', specialClass: 'gray-bg' }
+        })
         .state('index', {
             abstract: true,
             url: "/index",
-            templateUrl: "views/common/content.html",
+            templateUrl: "views/common/content.html"
         })
         .state('index.main', {
             url: "/main",
@@ -26,6 +31,30 @@ function config($stateProvider, $urlRouterProvider) {
             templateUrl: "views/minor.html",
             data: { pageTitle: 'Example view' }
         })
+        .state('commerce', {
+            abstract: true,
+            url: "/commerce",
+            templateUrl: "views/common/content.html",
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['bower_components/footable/footable.all.min.js', 'bower_components/footable/footable.core.css']
+                        },
+                        {
+                            name: 'ui.footable',
+                            files: ['bower_components/footable/angular-footable.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('commerce.products_grid', {
+            url: "/products_grid",
+            templateUrl: "views/ecommerce_products_grid.html",
+            data: { pageTitle: 'E-commerce grid' }
+        })
+
 }
 angular
     .module('inspinia')
