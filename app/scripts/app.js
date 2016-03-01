117339179295a34b98b590de8cb7cb16505a7c2b
Tobias Büschel <tobias.buschel.15@ucl.ac.uk>
2016-03-01 18:34:43 +0000

    added summernote

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,6 @@ var emart = angular.module('emart', [
     'ui.router',                    // Routing
     'ui.bootstrap',                 // Bootstrap
     'ui.footable',
-    'slick'
+    'slick',
+    'summernote'
 ]);
