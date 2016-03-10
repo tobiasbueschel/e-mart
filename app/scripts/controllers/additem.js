a2ad6e30a174ec8b11041c9ed619c5fad97ad5a9
Kimeshan Naidoo <kimeshan@gmail.com>
2016-03-10 00:30:16 +0000

    Added getAllRows generic PHP response to get rows from specified array of tables

--- app/scripts/controllers/additem.js
+++ app/scripts/controllers/additem.js
@@ -3,8 +3,28 @@
  */
 emart.controller('addItemCtrl', function ($scope, $http, $state) {
     $scope.data = {}; //creating new scope that can be used inside tabset
+    //Pull categories and conditions from database
+
+    //Let's pull categories
+    console.log("pulling categories and conditions...");
+    var request = $http({
+        method: "post",
+        url: "/scripts/php/getAllRows.php",
+        data: {
+            tables: ["category","itemcondition"]
+        },
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    /* Successful HTTP post request or not */
+    request.success(function (data) {
+        console.log("Response: ",data);
+    });
+
     $scope.data.categories = ["Electronics","Household Appliances","Shoes","Clothes","Computers"];
     $scope.data.conditions = ["Poor","Average","Good","Excellent","New"];
+
+
     $scope.data.addItem = function () {
         console.log("Inside adding item method...");
         console.log($scope.data.name,$scope.data.description);
