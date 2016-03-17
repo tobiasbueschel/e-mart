ad7a8447e441f0be020c834a7230499010289b4d
jk12767@bristol.ac.uk <kajmo7179>
2016-03-17 04:17:42 -0700

    crate bid complete...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,26 +7,5 @@ emart.controller('MainCtrl', function ($scope, $http, $state) {
     this.userName = 'Example user';
     this.helloText = 'Welcome in SeedProject';
     this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
-    var now = new Date();
-    (function () {
-        return request = $http({
-            method: "post",
-            url: "/scripts/php/dateCtrl.php",
-            data: {
-                userID: $cookies.userID
-            },
-            headers: { 'Content-Type': 'application/json' }
-        }).then(function (response) {
-            if (response!==0) { //if no error when fetching database rows
-                console.log(response);
-            }
-            else {
-                console.log("Error loading drop down menu conditions and categories from database");
-            }
-        });
-    })();
-
-
-
 
 });
