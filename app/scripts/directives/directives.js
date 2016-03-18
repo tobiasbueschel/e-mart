d4358cf9be840571c22003926e0cd14df23f1044
Tobias Büschel <tobias.buschel.15@ucl.ac.uk>
2016-03-18 21:11:46 +0000

    update

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -214,4 +214,52 @@ emart.directive('starRating', function () {
             }
         }
     }
-});
+})
+
+
+emart.directive('clockpicker', ['$timeout', '$parse', function ($timeout, $parse) {
+    return {
+        restrict: 'C',
+        require: 'ngModel',
+        link: function link(scope, elm, attrs, ngModel) {
+            var inputElm = $('input', elm);
+            var modelGetter = $parse(attrs['ngModel']);
+            var modelSetter = modelGetter.assign;
+
+            function afterUpdate() {
+                return $timeout(function () {
+                    var value = modelGetter(scope);
+                    if (value) {
+                        value = moment(value);
+                    }
+                    var inputVal = moment(inputElm.val(), 'HH:mm');
+                    if (inputVal.isValid()) {
+                        if (!value) {
+                            modelSetter(scope, inputVal.toDate());
+                        } else {
+                            value.hour(inputVal.hour());
+                            value.minute(inputVal.minute());
+
+                            modelSetter(scope, value.toDate());
+
+                            scope.$digest();
+                        }
+                    }
+                })
+            }
+
+            elm.clockpicker({
+                donetext: 'Done',
+                autoclose: true,
+                afterDone: afterUpdate
+            });
+            inputElm.blur(afterUpdate);
+            ngModel.$formatters.push(function (value) {
+                if (value) {
+                    inputElm.val(moment(value).format('HH:mm'));
+                }
+                return value;
+            });
+        }
+    }
+}]);
